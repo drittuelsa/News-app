6a6ca78940828c6a4b90a9f71db20a573dfb790f
Drittu Elsa Sebastian <134041017+drittuelsa@users.noreply.github.com>
2023-08-19 17:08:30 +0530

    Update action.js

--- src/News/action.js
+++ src/News/action.js
@@ -1,8 +1,8 @@
-const API_KEY = '8449c3372dbf42daa8ac80d825c83e83';
+// const API_KEY = '8449c3372dbf42daa8ac80d825c83e83';
 
 export function GetNews(){
     return(dispatch) =>{
-        fetch('https://newsapi.org/v2/everything?q=apple&from=2023-08-17&to=2023-08-17&sortBy=popularity&apiKey= ${API_KEY}')
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-07-19&sortBy=publishedAt&apiKey=8449c3372dbf42daa8ac80d825c83e83')
         .then(response =>response.json())
         .then(json =>{
             dispatch(GetByName(json.articles))
